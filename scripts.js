fbba443e32055e2ec7994916e81a93e57fe157d6
armourka <51520920+armourka@users.noreply.github.com>
2025-03-26 21:03:00 -0400

    update footer to match mockups

--- scripts.js
+++ scripts.js
@@ -1,27 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const menu = document.getElementById('menu');
-	const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const menuBtn = document.getElementById('menu-btn');
 
-	function toggleMenu() {
-		if (menu.classList.contains('hidden')) {
-			menu.classList.remove('hidden');
-			menuBtn.innerHTML = '<img src="assets/xmark.svg" alt="Close Menu" class="icon" />';
-		} else {
-			menu.classList.add('hidden');
-			menuBtn.innerHTML = '<img src="assets/bars.svg" alt="Menu" class="icon" />';
-		}
+function toggleMenu() {
+	if (menu.classList.contains('hidden')) {
+		show(menu);
+		menuBtn.innerHTML = '<img src="assets/xmark.svg" alt="Close Menu" class="icon" />';
+	} else {
+		hide(menu);
+		menuBtn.innerHTML = '<img src="assets/bars.svg" alt="Menu" class="icon" />';
 	}
+}
 
-	menuBtn.addEventListener('click', toggleMenu);
-});
+menuBtn.addEventListener('click', toggleMenu);
+
+function show(elt) {
+	elt.classList.remove('hidden')
+}
+
+function hide(elt) {
+	elt.classList.add('hidden')
+}
 
 document.getElementById('contact-form').addEventListener('submit', function (event) {
 	event.preventDefault();
 	alert('Form submitted!');
 });
 
-window.onscroll = function () {
+window.addEventListener("scroll", function () {
 	if (window.scrollY > 500) {
-		document.getElementById('back-to-top').style.display = "block";
+		show(document.getElementById('back-to-top'));
 	}
-};
+	else {
+		hide(document.getElementById('back-to-top'));
+	}
+}, false);
